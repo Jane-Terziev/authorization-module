7dab2b1e65ef355c716ea0e5a630a38558e6e3c0
Jane-Terziev <janeterziev@gmail.com>
2022-07-26 08:52:37 +0200

    Fixed wrong repository name in service

--- lib/authorization_module/identity_service.rb
+++ lib/authorization_module/identity_service.rb
@@ -2,12 +2,12 @@ module AuthorizationModule
   class IdentityService
     def initialize(
         identity_repository: AuthorizationModule::Config::AuthorizationConfiguration.identity_repository,
-        group_repository: AuthorizationModule::Config::AuthorizationConfiguration.group_repository,
+        user_group_repository: AuthorizationModule::Config::AuthorizationConfiguration.user_group_repository,
         role_repository: AuthorizationModule::Config::AuthorizationConfiguration.role_repository
     )
 
       self.identity_repository = identity_repository
-      self.group_repository = group_repository
+      self.user_group_repository = user_group_repository
       self.role_repository = role_repository
     end
 
@@ -15,7 +15,7 @@ module AuthorizationModule
       ActiveRecord::Base.transaction do
         identity_repository.create_new(
             id: command.id,
-            group_id: group_repository.find_by_name(command.group_name).map(&:id).or_else_raise(&StandardError.method(:new)),
+            group_id: user_group_repository.find_by_name(command.group_name).map(&:id).or_else_raise(&StandardError.method(:new)),
             role: role_repository.find_by_name(command.role_name).or_else_raise(&StandardError.method(:new))
         ).tap{ |identity| identity_repository.save!(identity) }
       end
@@ -23,6 +23,6 @@ module AuthorizationModule
 
     private
 
-    attr_accessor :identity_repository, :group_repository, :role_repository
+    attr_accessor :identity_repository, :user_group_repository, :role_repository
   end
 end
